0c741e183943a51b8a1357647cecc071fc6de604
kingson4wu <Kingson_Wu@163.com>
2020-04-24 21:20:01 +0800

    f

---
+++ Algorithm/invert-binary-tree.go
@@ -0,0 +1,56 @@
+package algorithm
+
+// TreeNode is ...
+//https://stackoverflow.com/questions/53004291/exported-type-should-have-comment-or-be-unexported-golang-vs-code
+//翻转一棵二叉树(左右翻转): https://leetcode-cn.com/problems/invert-binary-tree/
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	Temp := root.Left
+	root.Left = root.Right
+	root.Right = Temp
+
+	if root.Left != nil {
+		invertTree(root.Left)
+	}
+	if root.Right != nil {
+		invertTree(root.Right)
+	}
+	return root
+}
+
+/*func main() {
+	root := new(TreeNode)
+	root.Val = 3
+
+	invertTree(root)
+}*/
+
+/**
+自己写的，但是少了第一个root判空
+*/
+
+/**
+利用前序遍历
+class Solution {
+        // 先序遍历--从顶向下交换
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) return null;
+            // 保存右子树
+            TreeNode rightTree = root.right;
+            // 交换左右子树的位置
+            root.right = invertTree(root.left);
+            root.left = invertTree(rightTree);
+            return root;
+        }
+    }
+*/
+//这种写法可以不用temp
---
+++ Algorithm/power-of-two.go
@@ -0,0 +1,26 @@
+package algorithm
+
+/**
+给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+https://leetcode-cn.com/problems/power-of-two/
+*/
+func isPowerOfTwo(n int) bool {
+
+	count := 0
+	for n > 0 {
+		if n%2 == 1 {
+			count++
+			if count > 1 {
+				return false
+			}
+
+		}
+		n = n >> 1
+	}
+	return count == 1
+
+}
+
+/**
+自己做的
+*/
