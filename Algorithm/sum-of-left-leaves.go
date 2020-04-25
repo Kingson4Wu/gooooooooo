9f8948d219b19665455dd2758c97198b7536d899
kingson4wu <Kingson_Wu@163.com>
2020-04-25 16:51:22 +0800

    isBalanced

---
+++ Algorithm/sum-of-left-leaves.go
@@ -0,0 +1,30 @@
+package algorithm
+
+/**
+计算给定二叉树的所有左叶子之和。
+*/
+
+func sumOfLeftLeaves(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	sum := 0
+	if root.Left != nil {
+		if root.Left.Left == nil && root.Left.Right == nil {
+			sum += root.Left.Val
+		} else {
+			sum += sumOfLeftLeaves(root.Left)
+		}
+	}
+	if root.Right != nil {
+		sum += sumOfLeftLeaves(root.Right)
+	}
+	return sum
+}
+
+/**
+简单，自己写的
+1. 少了root非空判断
+2. left和right都为空才是叶子节点，少了right的判断
+*/
