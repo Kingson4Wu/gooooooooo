e7b3da711f501ca81fc48e8d106af3363703b25b
kingson4wu <kingson4wu@gmail.com>
2022-12-27 17:05:28 +0800

    f

--- designpattern/17_flyweight/flyweight_test.go
+++ designpattern/17_flyweight/flyweight_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+/**
+（一）概念
+
+享元是一种结构型设计模式，它允许你在消耗少量内存的情况下支持大量对象。
+
+
+
+模式通过共享多个对象的部分状态来实现上述功能。换句话来说，享元会将不同对象的相同数据进行缓存以节省内存。
+*/
+
 func TestFlyweight(t *testing.T) {
 	dispatcher := NewTaxiDispatcher("北京市出租车调度系统")
 	dispatcher.AddTrace("京B.123456", "黄色", "北京现代", "北汽", 10, 20)
--- designpattern/21_builder/builder_test.go
+++ designpattern/21_builder/builder_test.go
@@ -5,6 +5,17 @@ import (
 	"testing"
 )
 
+/**
+生成器模式
+
+（一）概念
+
+生成器是一种创建型设计模式，使你能够分步骤创建复杂对象。
+
+
+与其他创建型模式不同，生成器不要求产品拥有通用接口。这使得用相同的创建过程生成不同的产品成为可能。
+*/
+
 func TestBuilder(t *testing.T) {
 	pancakeCook := NewPancakeCook(NewNormalPancakeBuilder())
 	fmt.Printf("摊一个普通煎饼 %#v\n", pancakeCook.MakePancake())
